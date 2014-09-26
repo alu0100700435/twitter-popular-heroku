9ae766a9f2d0b744ea7f11ab58a5799fc00edfaa
Sergio Miguel Delgado <alu0100706468@ull.edu.es>
2014-09-26 17:19:12 +0100

    Añadiendo métodos de la clase TwitterPopular

--- twitter.rb
+++ twitter.rb
@@ -4,19 +4,37 @@ require 'sinatra'
 require './configure'
 
 class TwitterPopular
-	def user_t
+	def user_t(cliente)
+		cliente.user? self
 	end
 
 	def friends_t
+		self.friends_count
 	end
 
 	def imagen_t
+		self.profile_image_url
 	end
 
 	def get_amigos_t(num, amigos)
+		amigos_t = self.friend_ids(@name).attrs[:ids].take(10)
+		if (num < 10)
+				num.times do |i|
+						user_n = self.user(amigos_t[i])
+						usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i
+				end
+		end
+
+		if (num >= 10)
+				10.times do |i|
+						user_n = self.user(amigos_t[i])
+						usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i
+				end
+		end
 	end
 
 	def ordenar_t
+		self.sort_by {|user,followers| -followers}
 	end
 end
 
